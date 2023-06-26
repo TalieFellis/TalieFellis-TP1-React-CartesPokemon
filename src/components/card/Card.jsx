603fb773a2874b04a4f618d567d5f2fdd5d97af0
TalieF <taliefellis@gmail.com>
2023-06-26 10:01:34 -0400

    Ajout du display:none pour effet hover des habiletés

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import "./Card.css";
 import Image from '../image/Image';
 import CardTitle from '../card-title/CardTitle';
+import Abilities from '../abilities/Abilities';
 
 const Card = (props) => {
   const { image, name, abilities } = props.data;
